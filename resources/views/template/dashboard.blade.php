56a401ffd8ff2787e98a8ee2db6471a7c8504078
Ikbalhumen <masterplan2345@gmail.com>
2023-12-03 01:16:21 +0700

    tambah sidebar

--- resources/views/template/dashboard.blade.php
+++ resources/views/template/dashboard.blade.php
@@ -5,8 +5,7 @@
   <title>@yield('title')</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
   <!-- Tautan untuk Font Awesome CSS -->
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" integrity="sha512-xr1kPyRKYOTSwHSL1L2VgMowwA6fCcRVF1AqAd5gGQdfBz9VfUKb92ShyOH8LqBq5U4+CvA0o+6w9DTlVgKTLw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
   <!-- Masukkan CSS dan script lainnya -->
   <style>
     .sidebar {
@@ -71,8 +70,8 @@
   <div class="container-fluid body p-0 content" id="main-content">
     <nav class="navbar navbar-sm navbar-expand-lg navbar-fixed" id="navbar" style="background-color: #004225;">
       <div class="container-fluid">
-        <button class="sidebar-toggler bg-danger" onclick="toggleSidebar()">
-          <i class="fas fa-bars">bbb</i> <!-- Ikon sidebar -->
+        <button class="sidebar-toggler " onclick="toggleSidebar()">
+        <i class="fa-solid fa-list"></i>
         </button>
 
         <a class="navbar-brand text-white" href="#">
@@ -80,8 +79,29 @@
             {{env('APP_NAME')}}
           </b>
         </a>
+        
 
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+       
+
+        <div class="collapse navbar-collapse " id="navbarSupportedContent">
+          
+        <!-- Tambahkan tautan pengguna di sebelah kanan -->
+        <div class="ml-auto mr-lg-2 navbar-menu">
+          <button class="btn btn-light text-white" onclick="toggleUserMenu()">
+            <i class="fa fa-user"></i>&nbsp;joseph <i class="fa fa-angle-down"></i>
+          </button>
+          <div class="dropdown-menu dropdown-caret dropdown-menu-right dropdown-animated brc-primary-m3 py-1" id="userMenu" style="display: none;">
+            <a class="dropdown-item btn btn-outline-grey bgc-h-success-l3 btn-h-light-success btn-a-light-success" id="ganti-pass">
+              <i class="fa fa-key text-success-m1 text-105 mr-1"></i>
+              Ganti Password
+            </a>
+            <a class="dropdown-item btn btn-outline-grey bgc-h-secondary-l3 btn-h-light-secondary btn-a-light-secondary" href="{{ url('auth/logout') }}">
+              <i class="fa fa-power-off text-warning-d1 text-105 mr-1"></i>
+              Logout
+            </a>
+          </div>
+        </div>
           <!-- Tempat untuk menambahkan elemen lain di navbar -->
         </div>
       </div>
@@ -95,17 +115,27 @@
   @yield('inline_script')
 
   <!-- Tautan untuk Bootstrap JS (Pastikan Anda memasukkan jQuery dan Popper.js jika diperlukan) -->
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-KyZXEAg3QhqLMpG8r+Knujsl5+zgbY9v9E69Jwii8cAPlktgz5F3cJLve+8EvoB1" crossorigin="anonymous"></script>
+  <!-- Tautan untuk Bootstrap JS (Pastikan Anda memasukkan jQuery dan Popper.js jika diperlukan) -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-KyZXEAg3QhqLMpG8r+Knujsl5+zgbY9v9E69Jwii8cAPlktgz5F3cJLve+8EvoB1" crossorigin="anonymous"></script>
 
-  <!-- Script untuk toggle sidebar dan menggeser konten -->
-  <script>
-    function toggleSidebar() {
-      var sidebar = document.getElementById('sidebar');
-      var content = document.getElementById('content');
-      sidebar.classList.toggle('active');
-      content.classList.toggle('active');
-    }
-  </script>
+<script>
+  document.addEventListener("DOMContentLoaded", function() {
+    var userDropdown = document.getElementById('userDropdown');
+    userDropdown.addEventListener('click', function(event) {
+      event.stopPropagation();
+      var userMenu = document.getElementById('userDropdownMenu');
+      userMenu.classList.toggle('show');
+    });
+
+    document.addEventListener('click', function(event) {
+      var userMenu = document.getElementById('userDropdownMenu');
+      if (!userMenu.contains(event.target)) {
+        userMenu.classList.remove('show');
+      }
+    });
+  });
+</script>
+
 </body>
 
 </html>
