703155e0746c4472b46a8ee85c428e0bc027a36c
hadiar17 <hadiabdulrasyid7@gmail.com>
2023-12-09 22:39:25 +0700

    update

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,6 +1,6 @@
  <!-- To the right -->
  <div class="float-right d-none d-sm-inline">
-    Aufa ganteng
+    Teknik Informatika
   </div>
   <!-- Default to the left -->
   <strong>Copyright &copy; 2023-2024 Barudak UIN</a>.</strong> All rights reserved.
--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,10 @@ Route::get('/kinerjas', [KinerjaController::class, 'index'])->name('kinerjas.ind
 Route::resource('sasarans',SasaranController::class);
 Route::get('/sasarans', [SasaranController::class, 'index'])->name('sasarans.index');
 
+Route::resource('sasarans',SasaranController::class);
+Route::get('/', [SasaranController::class, 'index'])->name('sasarans.index');
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
